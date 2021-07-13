b9fdd637fcddbf80dc3960af83b55104dc85ef72
Danylo Maryskevych <danylo.maryskevych@gmail.com>
2021-07-14 00:04:58 +0300

    add Task4

--- src/main/java/com/epam/spring/core/config/AppConfigTwo.java
+++ src/main/java/com/epam/spring/core/config/AppConfigTwo.java
@@ -1,7 +1,18 @@
 package com.epam.spring.core.config;
 
+import com.epam.spring.core.beans3.BeanD;
+import com.epam.spring.core.beans3.BeanF;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
+
 @Configuration
+@ComponentScan(basePackages = "com.epam.spring.core.beans2")
+@ComponentScan(basePackageClasses = {BeanD.class, BeanF.class},
+        useDefaultFilters = false,
+        includeFilters = {
+                @ComponentScan.Filter(type = ASSIGNABLE_TYPE, value = {BeanD.class, BeanF.class}),
+        })
 public class AppConfigTwo {
 }
